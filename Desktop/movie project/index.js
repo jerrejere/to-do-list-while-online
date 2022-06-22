d6616aff0820e26552fab64314677ab112eb4c9d
jerryjohns <jerry.johns@student.moringaschool.com>
2022-06-22 09:14:28 +0300

    add

--- Desktop/movie project/index.js
+++ Desktop/movie project/index.js
@@ -15,3 +15,21 @@ axios.request(options).then(function (response) {
 }).catch(function (error) {
 	console.error(error);
 });
+const carouselSlide= document.querySelector('carousel-slide');
+const carouselImages= document.querySelector('carousel-slide img')
+
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+let counter = 1 ;
+const Size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translatex ('+ (-size * counter) + 'px)'
+
+nextBtn.addEventListener('click',()=>{
+  carouselSlide.style.transition ="tranform 0.4s ease-in-out"
+  counter++;
+  console.log(counter)
+  carouselSlide.style.transform = 'translatex ('+ (-size * counter) + 'px)'
+}
+)
